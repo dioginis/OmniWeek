f2b3bc0441086883f31ce5c654e0c66b73b17825
Dioginis <dioginiscarvalho@hotmail.com>
2019-06-12 20:02:07 -0300

    feat: add new post

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import api from "../services/api";
 
 import "./Feed.css";
 
@@ -8,33 +9,44 @@ import comment from "../assets/comment.svg";
 import send from "../assets/send.svg";
 
 class Feed extends Component {
+  state = {
+    feed: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("posts");
+
+    this.setState({ feed: response.data });
+  }
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>Dioginis Carvalho</span>
-              <span className="place">Campos</span>
-            </div>
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-            <img src={more} alt="Mais" />
-          </header>
-          <img src="http://locahost:3333/files/OmniStackWallpaper.png" alt="" />
+              <img src={more} alt="Mais" />
+            </header>
+            <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="" />
-              <img src={comment} alt="" />
-              <img src={send} alt="" />
-            </div>
-            <strong>900 curtidas</strong>
-            <p>
-              Primeiro post
-              <span>#react #rocketseat</span>
-            </p>
-          </footer>
-        </article>
+            <footer>
+              <div className="actions">
+                <img src={like} alt="" />
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
+              <strong>{post.likes}curtidas </strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
