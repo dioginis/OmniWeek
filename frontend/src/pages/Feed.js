e4c56c2351beac8c337523d15d32fe75b6c008ce
Dioginis <dioginiscarvalho@hotmail.com>
2019-06-12 16:33:54 -0300

    feat: create feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 
+import "./Feed.css";
+
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
+
 class Feed extends Component {
   render() {
-    return <h1>Feed</h1>;
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Dioginis Carvalho</span>
+              <span className="place">Campos</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+          <img src="http://locahost:3333/files/OmniStackWallpaper.png" alt="" />
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+            <strong>900 curtidas</strong>
+            <p>
+              Primeiro post
+              <span>#react #rocketseat</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
   }
 }
 
